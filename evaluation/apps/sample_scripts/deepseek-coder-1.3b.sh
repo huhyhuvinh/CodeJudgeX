de38c1acf973e3ea060460243a390d12ab9ca384
Kien <nguyenhoangtrungkien92@gmail.com>
2025-11-09 13:16:42 +0700

    feat: adđ model deepseek-ai/deepseek-coder-1.3b-instruct

---
+++ evaluation/apps/sample_scripts/deepseek-coder-1.3b.sh
@@ -0,0 +1,74 @@
+source ../venv/bin/activate
+python ./apps/code_score.py \
+--test_case gpt \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 2 \
+--compare_prompt 0 \
+--analyze_prompt 0 \
+--temperature 0.4 \
+--return_type bool \
+--num_samples 3
+
+python ./apps/code_score.py \
+--test_case gpt \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 2 \
+--compare_prompt 1 \
+--analyze_prompt 0 \
+--temperature 0.4 \
+--return_type bool \
+--num_samples 3
+
+python ./apps/code_score.py \
+--test_case gpt \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 0 \
+--temperature 0.4 \
+--return_type bool \
+--num_samples 3
+
+python ./apps/code_score.py \
+--test_case gpt \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 1 \
+--temperature 0.4 \
+--return_type bool \
+--num_samples 3
+
+python ./apps/code_score.py \
+--test_case gpt \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 2 \
+--temperature 0.4 \
+--return_type "0_to_4_score_functional_correctness" \
+--num_samples 3
+
+python ./apps/code_score.py \
+--test_case gpt \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 3 \
+--temperature 0.4 \
+--return_type "0_to_4_score_functional_correctness" \
+--num_samples 3
+
+python ./apps/code_score.py \
+--test_case gpt \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 4 \
+--temperature 0.4 \
+--return_type "inconsistency_level" \
+--num_samples 3
+
+python ./apps/code_score.py \
+--test_case gpt \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 5 \
+--temperature 0.4 \
+--return_type "inconsistency_level" \
+--num_samples 3
