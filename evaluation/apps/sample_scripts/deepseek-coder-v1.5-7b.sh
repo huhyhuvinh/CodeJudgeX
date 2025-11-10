edcead5c226f3ed7ff0a9c35b4646b73abd610a2
Kien <nguyenhoangtrungkien92@gmail.com>
2025-11-10 15:34:31 +0700

    update: comment step 1 of deepseek coder

--- evaluation/apps/sample_scripts/deepseek-coder-v1.5-7b.sh
+++ evaluation/apps/sample_scripts/deepseek-coder-v1.5-7b.sh
@@ -19,56 +19,56 @@ python ./apps/code_score.py \
 --return_type bool \
 --num_samples 3
 
-python ./apps/code_score.py \
---test_case gpt \
---model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
---step 1 \
---compare_prompt 0 \
---temperature 0.4 \
---return_type bool \
---num_samples 3
+# python ./apps/code_score.py \
+# --test_case gpt \
+# --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+# --step 1 \
+# --compare_prompt 0 \
+# --temperature 0.4 \
+# --return_type bool \
+# --num_samples 3
 
-python ./apps/code_score.py \
---test_case gpt \
---model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
---step 1 \
---compare_prompt 1 \
---temperature 0.4 \
---return_type bool \
---num_samples 3
+# python ./apps/code_score.py \
+# --test_case gpt \
+# --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+# --step 1 \
+# --compare_prompt 1 \
+# --temperature 0.4 \
+# --return_type bool \
+# --num_samples 3
 
-python ./apps/code_score.py \
---test_case gpt \
---model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
---step 1 \
---compare_prompt 2 \
---temperature 0.4 \
---return_type "0_to_4_score_functional_correctness" \
---num_samples 3
+# python ./apps/code_score.py \
+# --test_case gpt \
+# --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+# --step 1 \
+# --compare_prompt 2 \
+# --temperature 0.4 \
+# --return_type "0_to_4_score_functional_correctness" \
+# --num_samples 3
 
-python ./apps/code_score.py \
---test_case gpt \
---model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
---step 1 \
---compare_prompt 3 \
---temperature 0.4 \
---return_type "0_to_4_score_functional_correctness" \
---num_samples 3
+# python ./apps/code_score.py \
+# --test_case gpt \
+# --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+# --step 1 \
+# --compare_prompt 3 \
+# --temperature 0.4 \
+# --return_type "0_to_4_score_functional_correctness" \
+# --num_samples 3
 
-python ./apps/code_score.py \
---test_case gpt \
---model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
---step 1 \
---compare_prompt 4 \
---temperature 0.4 \
---return_type "inconsistency_level" \
---num_samples 3
+# python ./apps/code_score.py \
+# --test_case gpt \
+# --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+# --step 1 \
+# --compare_prompt 4 \
+# --temperature 0.4 \
+# --return_type "inconsistency_level" \
+# --num_samples 3
 
-python ./apps/code_score.py \
---test_case gpt \
---model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
---step 1 \
---compare_prompt 5 \
---temperature 0.4 \
---return_type "inconsistency_level" \
---num_samples 3
+# python ./apps/code_score.py \
+# --test_case gpt \
+# --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+# --step 1 \
+# --compare_prompt 5 \
+# --temperature 0.4 \
+# --return_type "inconsistency_level" \
+# --num_samples 3
