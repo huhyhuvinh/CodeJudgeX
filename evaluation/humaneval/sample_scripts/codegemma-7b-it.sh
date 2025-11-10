5eb37b5202040311429e10729c113adfff9b44e0
huhyhuvinh <vinhtran048@gmail.com>
2025-11-10 11:20:28 +0000

    feat: added codegemma evaluation

---
+++ evaluation/humaneval/sample_scripts/codegemma-7b-it.sh
@@ -0,0 +1,23 @@
+source ../venv/bin/activate
+
+python ./humaneval/code_score.py \
+--test_case python-small-test \
+--model google/codegemma-7b-it \
+--step 2 \
+--compare_prompt 0 \
+--analyze_prompt 0 \
+--temperature 0.4 \
+--with_prefix \
+--return_type bool \
+--num_samples 3
+
+python ./humaneval/code_score.py \
+--test_case python-small-test \
+--model google/codegemma-7b-it \
+--step 2 \
+--compare_prompt 1 \
+--analyze_prompt 0 \
+--temperature 0.4 \
+--with_prefix \
+--return_type bool \
+--num_samples 3
