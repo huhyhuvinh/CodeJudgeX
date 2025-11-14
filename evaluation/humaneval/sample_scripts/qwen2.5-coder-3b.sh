5b2180dfa2c1e6dd942122a896e26af6ecc09416
Kien <nguyenhoangtrungkien92@gmail.com>
2025-11-14 15:06:50 +0700

    refactor: comment out inconsistency level method of qwen in humaneval

--- evaluation/humaneval/sample_scripts/qwen2.5-coder-3b.sh
+++ evaluation/humaneval/sample_scripts/qwen2.5-coder-3b.sh
@@ -1,23 +1,23 @@
 source ../venv/bin/activate
-python ./humaneval/code_score.py \
---test_case python-small-test \
---model Qwen/Qwen2.5-Coder-3B-Instruct \
---step 1 \
---compare_prompt 4 \
---temperature 0.4 \
---with_prefix \
---return_type "inconsistency_level" \
---num_samples 3
+# python ./humaneval/code_score.py \
+# --test_case python-small-test \
+# --model Qwen/Qwen2.5-Coder-3B-Instruct \
+# --step 1 \
+# --compare_prompt 4 \
+# --temperature 0.4 \
+# --with_prefix \
+# --return_type "inconsistency_level" \
+# --num_samples 3
 
-python ./humaneval/code_score.py \
---test_case python-small-test \
---model Qwen/Qwen2.5-Coder-3B-Instruct \
---step 1 \
---compare_prompt 5 \
---temperature 0.4 \
---with_prefix \
---return_type "inconsistency_level" \
---num_samples 3
+# python ./humaneval/code_score.py \
+# --test_case python-small-test \
+# --model Qwen/Qwen2.5-Coder-3B-Instruct \
+# --step 1 \
+# --compare_prompt 5 \
+# --temperature 0.4 \
+# --with_prefix \
+# --return_type "inconsistency_level" \
+# --num_samples 3
 
 # python ./humaneval/code_score.py \
 # --test_case python-small-test \
