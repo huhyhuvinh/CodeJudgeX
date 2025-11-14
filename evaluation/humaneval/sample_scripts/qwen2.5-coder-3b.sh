9e041ecb551b6bce6080f3161505803857faaf9b
Kien <nguyenhoangtrungkien92@gmail.com>
2025-11-14 20:38:38 +0700

    fix: case for bigcodebench and humaneval

--- evaluation/humaneval/sample_scripts/qwen2.5-coder-3b.sh
+++ evaluation/humaneval/sample_scripts/qwen2.5-coder-3b.sh
@@ -1,23 +1,23 @@
 source ../venv/bin/activate
-# python ./humaneval/code_score.py \
-# --test_case python-small-test \
-# --model Qwen/Qwen2.5-Coder-3B-Instruct \
-# --step 1 \
-# --compare_prompt 4 \
-# --temperature 0.4 \
-# --with_prefix \
-# --return_type "inconsistency_level" \
-# --num_samples 3
+python ./humaneval/code_score.py \
+--test_case python-small-test \
+--model Qwen/Qwen2.5-Coder-3B-Instruct \
+--step 1 \
+--compare_prompt 4 \
+--temperature 0.4 \
+--with_prefix \
+--return_type "inconsistency_level" \
+--num_samples 3
 
-# python ./humaneval/code_score.py \
-# --test_case python-small-test \
-# --model Qwen/Qwen2.5-Coder-3B-Instruct \
-# --step 1 \
-# --compare_prompt 5 \
-# --temperature 0.4 \
-# --with_prefix \
-# --return_type "inconsistency_level" \
-# --num_samples 3
+python ./humaneval/code_score.py \
+--test_case python-small-test \
+--model Qwen/Qwen2.5-Coder-3B-Instruct \
+--step 1 \
+--compare_prompt 5 \
+--temperature 0.4 \
+--with_prefix \
+--return_type "inconsistency_level" \
+--num_samples 3
 
 # python ./humaneval/code_score.py \
 # --test_case python-small-test \
@@ -30,13 +30,13 @@ source ../venv/bin/activate
 # --return_type bool \
 # --num_samples 3
 
-python ./humaneval/code_score.py \
---test_case python-small-test \
---model Qwen/Qwen2.5-Coder-3B-Instruct \
---step 2 \
---compare_prompt 1 \
---analyze_prompt 0 \
---temperature 0.4 \
---with_prefix \
---return_type bool \
---num_samples 3
+# python ./humaneval/code_score.py \
+# --test_case python-small-test \
+# --model Qwen/Qwen2.5-Coder-3B-Instruct \
+# --step 2 \
+# --compare_prompt 1 \
+# --analyze_prompt 0 \
+# --temperature 0.4 \
+# --with_prefix \
+# --return_type bool \
+# --num_samples 3
