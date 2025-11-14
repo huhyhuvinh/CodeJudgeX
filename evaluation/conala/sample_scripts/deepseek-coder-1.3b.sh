aa6d8ecd3ab8d436aaf855b5f754fa4752e4444f
huhyhuvinh <vinhtran048@gmail.com>
2025-11-14 07:47:32 +0000

    chore: added and removed some test cases

--- evaluation/conala/sample_scripts/deepseek-coder-1.3b.sh
+++ evaluation/conala/sample_scripts/deepseek-coder-1.3b.sh
@@ -1,66 +1,34 @@
 source ../venv/bin/activate
-# python ./conala/code_score.py \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 0 \
-# --temperature 0.4 \
-# --return_type "helpful_score" \
-# --num_samples 3
+python ./conala/code_score.py \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 4 \
+--temperature 0.4 \
+--return_type "inconsistency_level" \
+--num_samples 3
+
+python ./conala/code_score.py \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 5 \
+--temperature 0.4 \
+--return_type "inconsistency_level" \
+--num_samples 3
 
 # python ./conala/code_score.py \
 # --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 1 \
-# --temperature 0.4 \
-# --return_type "helpful_score" \
-# --num_samples 3
-
-# python ./conala/code_score.py \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
+# --step 2 \
 # --compare_prompt 2 \
+# --analyze_prompt 0 \
 # --temperature 0.4 \
-# --return_type "0_to_4_score_usefulness" \
+# --return_type helpful_score \
 # --num_samples 3
 
 # python ./conala/code_score.py \
 # --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
+# --step 2 \
 # --compare_prompt 3 \
+# --analyze_prompt 0 \
 # --temperature 0.4 \
-# --return_type "0_to_4_score_usefulness" \
+# --return_type helpful_score \
 # --num_samples 3
-
-# python ./conala/code_score.py \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 4 \
-# --temperature 0.4 \
-# --return_type "inconsistency_level" \
-# --num_samples 3
-
-# python ./conala/code_score.py \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 5 \
-# --temperature 0.4 \
-# --return_type "inconsistency_level" \
-# --num_samples 3
-
-python ./conala/code_score.py \
---model deepseek-ai/deepseek-coder-1.3b-instruct \
---step 2 \
---compare_prompt 2 \
---analyze_prompt 0 \
---temperature 0.4 \
---return_type helpful_score \
---num_samples 3
-
-python ./conala/code_score.py \
---model deepseek-ai/deepseek-coder-1.3b-instruct \
---step 2 \
---compare_prompt 3 \
---analyze_prompt 0 \
---temperature 0.4 \
---return_type helpful_score \
---num_samples 3
--- evaluation/humaneval/sample_scripts/deepseek-coder-1.3b.sh
+++ evaluation/humaneval/sample_scripts/deepseek-coder-1.3b.sh
@@ -1,63 +1,23 @@
 source ../venv/bin/activate
-# python ./humaneval/code_score.py \
-# --test_case python-small-test \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 0 \
-# --temperature 0.4 \
-# --with_prefix \
-# --return_type bool \
-# --num_samples 3
+python ./humaneval/code_score.py \
+--test_case python-small-test \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 4 \
+--temperature 0.4 \
+--with_prefix \
+--return_type "inconsistency_level" \
+--num_samples 3
 
-# python ./humaneval/code_score.py \
-# --test_case python-small-test \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 1 \
-# --temperature 0.4 \
-# --with_prefix \
-# --return_type bool \
-# --num_samples 3
-
-# python ./humaneval/code_score.py \
-# --test_case python-small-test \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 2 \
-# --temperature 0.4 \
-# --with_prefix \
-# --return_type "0_to_4_score_functional_correctness" \
-# --num_samples 3
-
-# python ./humaneval/code_score.py \
-# --test_case python-small-test \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 3 \
-# --temperature 0.4 \
-# --with_prefix \
-# --return_type "0_to_4_score_functional_correctness" \
-# --num_samples 3
-
-# python ./humaneval/code_score.py \
-# --test_case python-small-test \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 4 \
-# --temperature 0.4 \
-# --with_prefix \
-# --return_type "error_level" \
-# --num_samples 3
-
-# python ./humaneval/code_score.py \
-# --test_case python-small-test \
-# --model deepseek-ai/deepseek-coder-1.3b-instruct \
-# --step 1 \
-# --compare_prompt 5 \
-# --temperature 0.4 \
-# --with_prefix \
-# --return_type "error_level" \
-# --num_samples 3
+python ./humaneval/code_score.py \
+--test_case python-small-test \
+--model deepseek-ai/deepseek-coder-1.3b-instruct \
+--step 1 \
+--compare_prompt 5 \
+--temperature 0.4 \
+--with_prefix \
+--return_type "inconsistency_level" \
+--num_samples 3
 
 python ./humaneval/code_score.py \
 --test_case python-small-test \
