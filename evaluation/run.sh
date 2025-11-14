9fc271742e484f0dafe48c8a745afb9b79019434
Kien <nguyenhoangtrungkien92@gmail.com>
2025-11-14 15:46:46 +0700

    feat: add qwen and deepseek to run.sh

--- evaluation/run.sh
+++ evaluation/run.sh
@@ -7,5 +7,9 @@ export HF_TOKEN="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"
 cd evaluation
 bash ./conala/sample_scripts/llama-3-8b.sh
 bash ./conala/sample_scripts/codellama-7b.sh
+bash ./conala/sample_scripts/deepseek-coder-v1.5-7b.sh
+bash ./conala/sample_scripts/qwen2.5-coder-3b.sh
 bash ./bigcodebench/sample_scripts/llama-3-8b.sh
 bash ./bigcodebench/sample_scripts/codellama-7b.sh
+bash ./bigcodebench/sample_scripts/deepseek-coder-v1.5-7b.sh
+bash ./bigcodebench/sample_scripts/qwen2.5-coder-3b.sh
