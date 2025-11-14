b7aff9042ce2ebc2702c86b861d4905b534911cf
huhyhuvinh <vinhtran048@gmail.com>
2025-11-14 08:27:36 +0000

    chore: added codellama 7b

---
+++ evaluation/bigcodebench/sample_scripts/codellama-7b.sh
@@ -0,0 +1,37 @@
+python ./bigcodebench/code_score.py \
+--test_case codellama--CodeLlama-13b-Instruct-hf \
+--model meta-llama/CodeLlama-7b-Instruct-hf \
+--step 2 \
+--compare_prompt 1 \
+--analyze_prompt 0 \
+--temperature 0.4 \
+--return_type bool \
+--num_samples 3
+
+python ./bigcodebench/code_score.py \
+--test_case codellama--CodeLlama-13b-Instruct-hf \
+--model meta-llama/CodeLlama-7b-Instruct-hf \
+--step 2 \
+--compare_prompt 0 \
+--analyze_prompt 0 \
+--temperature 0.4 \
+--return_type bool \
+--num_samples 3
+
+python ./bigcodebench/code_score.py \
+--test_case codellama--CodeLlama-13b-Instruct-hf \
+--model meta-llama/CodeLlama-7b-Instruct-hf \
+--step 1 \
+--compare_prompt 4 \
+--temperature 0.4 \
+--return_type "inconsistency_level" \
+--num_samples 3
+
+python ./bigcodebench/code_score.py \
+--test_case codellama--CodeLlama-13b-Instruct-hf \
+--model meta-llama/CodeLlama-7b-Instruct-hf \
+--step 1 \
+--compare_prompt 5 \
+--temperature 0.4 \
+--return_type "inconsistency_level" \
+--num_samples 3
---
+++ evaluation/conala/sample_scripts/codellama-7b.sh
@@ -0,0 +1,33 @@
+python ./conala/code_score.py \
+--model meta-llama/CodeLlama-7b-Instruct-hf \
+--step 1 \
+--compare_prompt 4 \
+--temperature 0.4 \
+--return_type "inconsistency_level" \
+--num_samples 3
+
+python ./conala/code_score.py \
+--model meta-llama/CodeLlama-7b-Instruct-hf \
+--step 1 \
+--compare_prompt 5 \
+--temperature 0.4 \
+--return_type "inconsistency_level" \
+--num_samples 3
+
+# python ./conala/code_score.py \
+# --model meta-llama/CodeLlama-7b-Instruct-hf \
+# --step 2 \
+# --compare_prompt 2 \
+# --analyze_prompt 0 \
+# --temperature 0.4 \
+# --return_type helpful_score \
+# --num_samples 3
+
+# python ./conala/code_score.py \
+# --model meta-llama/CodeLlama-7b-Instruct-hf \
+# --step 2 \
+# --compare_prompt 3 \
+# --analyze_prompt 0 \
+# --temperature 0.4 \
+# --return_type helpful_score \
+# --num_samples 3
