c7293ecd1c2f25c80de71de32e73d8c8d15e97dc
Kien <nguyenhoangtrungkien92@gmail.com>
2025-11-15 12:57:06 +0700

    chore: comment out tested case of deepseek 7b in humaneval

--- evaluation/humaneval/sample_scripts/deepseek-coder-v.15-7b.sh
+++ evaluation/humaneval/sample_scripts/deepseek-coder-v.15-7b.sh
@@ -1,13 +1,13 @@
 source ../venv/bin/activate
-python ./humaneval/code_score.py \
---test_case python-small-test \
---model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
---step 1 \
---compare_prompt 4 \
---temperature 0.4 \
---with_prefix \
---return_type "inconsistency_level" \
---num_samples 3
+# python ./humaneval/code_score.py \
+# --test_case python-small-test \
+# --model deepseek-ai/deepseek-coder-7b-instruct-v1.5 \
+# --step 1 \
+# --compare_prompt 4 \
+# --temperature 0.4 \
+# --with_prefix \
+# --return_type "inconsistency_level" \
+# --num_samples 3
 
 python ./humaneval/code_score.py \
 --test_case python-small-test \
@@ -17,7 +17,7 @@ python ./humaneval/code_score.py \
 --temperature 0.4 \
 --with_prefix \
 --return_type "inconsistency_level" \
---num_samples 3
+--num_samples 2
 
 python ./humaneval/code_score.py \
 --test_case python-small-test \
@@ -28,7 +28,7 @@ python ./humaneval/code_score.py \
 --temperature 0.4 \
 --with_prefix \
 --return_type bool \
---num_samples 3
+--num_samples 1
 
 python ./humaneval/code_score.py \
 --test_case python-small-test \
